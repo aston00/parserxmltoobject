ed99aa3bc83c4e06b995cf83ae5a4e26f863f327
aston00 <jacksterwow@gmail.com>
2018-07-16 21:45:22 +0300

    parser from xml to js object

---
+++ app.js
@@ -0,0 +1,145 @@
+// let xml = '<?xml version="1.0" encoding="utf-8"?><audit><record><account>80ltks-yhfls5-24zyf2</account><token>qa@keepitqa.com</token><message>Signed in</message><allowed>true</allowed><client-ip>212.97.130.83</client-ip><time>2018-07-11 06:05:27.09406+00</time></record><record><account>80ltks-yhfls5-24zyf2</account><token>qa@keepitqa.com</token><message>Signed in</message><allowed>true</allowed><client-ip>212.97.130.83</client-ip><time>2018-07-11 05:49:12.849992+00</time></record><record><account>80ltks-yhfls5-24zyf2</account><token>qa@keepitqa.com</token><message>Signed in</message><allowed>true</allowed><client-ip>212.97.130.83</client-ip><time>2018-07-11 05:49:12.847489+00</time></record></audit>';
+// let xml = '<?xml version="1.0" encoding="UTF-8"?><CATALOG><CD><TITLE>Empire Burlesque</TITLE><ARTIST>Bob Dylan</ARTIST><COUNTRY>USA</COUNTRY>    <COMPANY>Columbia</COMPANY><PRICE>10.90</PRICE><YEAR>1985</YEAR></CD><CD><TITLE>Hide your heart</TITLE><ARTIST>Bonnie Tyler</ARTIST>  <COUNTRY>UK</COUNTRY>    <COMPANY>CBS Records</COMPANY>   <PRICE>9.90</PRICE>   <YEAR>1988</YEAR>  </CD>  <CD>    <TITLE>Greatest Hits</TITLE>    <ARTIST>Dolly Parton</ARTIST>    <COUNTRY>USA</COUNTRY>    <COMPANY>RCA</COMPANY>    <PRICE>9.90</PRICE>    <YEAR>1982</YEAR>  </CD>  <CD>    <TITLE>Still got the blues</TITLE>    <ARTIST>Gary Moore</ARTIST>    <COUNTRY>UK</COUNTRY>    <COMPANY>Virgin records</COMPANY>    <PRICE>10.20</PRICE>    <YEAR>1990</YEAR>  </CD>  <CD>    <TITLE>Eros</TITLE>    <ARTIST>Eros Ramazzotti</ARTIST>    <COUNTRY>EU</COUNTRY>    <COMPANY>BMG</COMPANY>    <PRICE>9.90</PRICE>    <YEAR>1997</YEAR>  </CD>  <CD>    <TITLE>One night only</TITLE>    <ARTIST>Bee Gees</ARTIST>    <COUNTRY>UK</COUNTRY>    <COMPANY>Polydor</COMPANY>    <PRICE>10.90</PRICE>    <YEAR>1998</YEAR>  </CD>  <CD>    <TITLE>Sylvias Mother</TITLE>    <ARTIST>Dr.Hook</ARTIST>    <COUNTRY>UK</COUNTRY>    <COMPANY>CBS</COMPANY>    <PRICE>8.10</PRICE>    <YEAR>1973</YEAR>  </CD>  <CD>    <TITLE>Maggie May</TITLE>    <ARTIST>Rod Stewart</ARTIST>    <COUNTRY>UK</COUNTRY>    <COMPANY>Pickwick</COMPANY>    <PRICE>8.50</PRICE>    <YEAR>1990</YEAR>  </CD>  <CD>    <TITLE>Unchain my heart</TITLE>    <ARTIST>Joe Cocker</ARTIST><COUNTRY>USA</COUNTRY>    <COMPANY>EMI</COMPANY>    <PRICE>8.20</PRICE>    <YEAR>1987</YEAR>  </CD></CATALOG>';
+// let xml = '<?xml version="1.0" encoding="utf-8"?><root>  <solution>    <will>protection</will>    <hot>-1375985428</hot>    <power>1022883804.8478293</power>    <reason>no</reason>    <fast>      <concerned>452297172.5341954</concerned>      <lion>1863961913</lion>      <use>triangle</use>      <brush>1133045794</brush>      <pink>find</pink>      <shirt>-874232233</shirt>    </fast>    <largest>2125014832</largest>  </solution>  <separate>-1743991643.220058</separate>  <three>mainly</three>  <herd>little</herd>  <other>-559519452</other>  <piano>-1302567489.0910096</piano></root>';
+
+// let xml ='<root><cut>-1247349990.1736937</cut><ocean>1687211542</ocean><together>1743304564</together><never>-1820303943</never><hide>1984221464.8831573</hide><kids><represent><foreign>-1847073554</foreign><willing>-927572322</willing><continent>1434977943.8947005</continent><describe>-159051158.39184618</describe><officer><tell>988964421.4483795</tell><rush>temperature</rush><naturally>-1410158501</naturally><eventually>close</eventually><welcome><volume>month</volume><angry><fix>finger</fix><pour><watch><neck>292435805</neck><form>structure</form><danger><pupil>acres</pupil><half><kill><peace><foot>-1546930758</foot><worried>1272109000</worried><fully>1087478811</fully><general>618020700</general><comfortable><master>-880659196.165843</master><struggle><statement>-1984098918</statement><lonely>finish</lonely><butter>traffic</butter><drive>-486403776</drive><warm>-470335717</warm><prepare>no</prepare><pass>-2053081144</pass><apart>cross</apart><unknown>488293959.5112772</unknown><copper>-1396338745</copper><policeman>1269324150.8190498</policeman></struggle><rather>read</rather><music>924860316</music><her>build</her><energy>slow</energy><farmer>report</farmer><object>rich</object><effect>shine</effect><tree>-715020880</tree><activity>teacher</activity></comfortable><leave>-760707073.1177645</leave><tone>satisfied</tone><syllable>198748647.00836945</syllable><deer>180957578.99283838</deer><rubbed>185362379.65277958</rubbed><influence>41255587</influence></peace><teeth>drive</teeth><accident>-459703038.89764357</accident><command>1412412422</command><slope>throw</slope><connected>tent</connected><come>rod</come><bar>with</bar><couple>1623724947</couple><heading>shelf</heading><wash>area</wash></kill><compare>bit</compare><changing>2080545878</changing><respect>-258045492</respect><electric>hit</electric><fed>source</fed><numeral>-205643285</numeral><affect>before</affect><tank>1811671239</tank><classroom>-1686875106</classroom><conversation>service</conversation></half><putting>triangle</putting><political>ranch</political><strength>842821490</strength><locate>surrounded</locate><adult>chance</adult><life>1948112403.878995</life><identity>128993135</identity><out>clear</out><melted>926381022.9876032</melted></danger><change>-10171869.470674038</change><daily>918637509.6297255</daily><its>arm</its><shot>1504981283</shot><flight>-1600700697</flight><nose>design</nose><grew>more</grew><river>707409499.5126667</river></watch><put>reader</put><rich>1222487622</rich><still>-1790457258.9075193</still><came>spider</came><pocket>grow</pocket><became>-86758422.93857479</became><tool>pencil</tool><barn>ice</barn><sold>frequently</sold><divide>again</divide></pour><short>1975768719</short><desert>firm</desert><development>303022852.02955246</development><worker>-567863676</worker><child>tongue</child><wear>-22382723</wear><wise>132658230.96214008</wise><declared>carry</declared><cloud>-1953948927.5130448</cloud></angry><safety>-534887784</safety><range>themselves</range><surface>prize</surface><daily>1025636197</daily><lying>spirit</lying><income>1028615286.5990252</income><by>1189956443</by><studying>950487603.7581086</studying><valley>managed</valley></welcome><more>studying</more><built>among</built><bottom>main</bottom><fear>nose</fear><bag>them</bag><speed>telephone</speed></officer><crop>tonight</crop><seems>grandmother</seems><column>-1703789175</column><known>evening</known><brown>bright</brown><dust>century</dust></represent><writing>-1766446769.252171</writing><being>division</being><raw>create</raw><anyway>dry</anyway><action>568852095.3378134</action><village>1967611677</village><lift>521110989</lift><brought>-624231518</brought><spread>1297447422</spread><tank>-132214965.12732697</tank></kids><rear>choice</rear><knowledge>-1095532333.476737</knowledge><egg>warn</egg><hair>631085910.664278</hair><origin>whole</origin></root>';
+
+let xml = '<?xml version="1.0" encoding="UTF-8" ?><root>	<report>		<rather>			<enough>				<color>half</color>				<enter>tales</enter>				<knowledge>thought</knowledge>				<around>-1720875258</around>				<ancient>still</ancient>			</enough>			<enough>				<color>half</color>				<enter>tales</enter>				<knowledge>thought</knowledge>				<around>-1720875258</around>				<ancient>still</ancient>			</enough>      <enough>				<color>half</color>				<enter>tales</enter>				<knowledge>thought</knowledge>				<around>-1720875258</around>				<ancient>still</ancient>			</enough>		</rather>		<damage>garage</damage>		<mouth>-556390448</mouth>		<buried>-2061356173.5910926</buried>		<bat>-1750002269.0090003</bat>	</report>	<event>merely</event>	<expect>hollow</expect>	<piano>camp</piano>	<new>nearly</new></root>';
+
+let parser = new DOMParser();
+let parsedFromString = parser.parseFromString(xml, "text/xml");
+
+
+let resultObject = {};
+let parsedElement = parsedFromString.documentElement;
+transformToXML(parsedElement);
+
+function transformToXML(xml) {
+
+    let object = {};
+    if (xml.parentNode.nodeType == 9) {
+        if (Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+            object[xml.children[0].nodeName] = [];
+            for (let i = 0; i < xml.children.length; i++) {
+                object[xml.children[0].nodeName].push(transformToXML(xml.children[i]));
+            }
+        } else {
+            for (let i = 0; i < xml.children.length; i++) {
+                object[xml.children[i].nodeName] = transformToXML(xml.children[i]);
+            }
+        }
+        resultObject[xml.nodeName] = object;
+    } else if (xml.children.length > 1 && Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+        console.log(xml);
+        object[xml.children[0].nodeName] = [];
+        for (let i = 0; i < xml.children.length; i++) {
+            object[xml.children[0].nodeName].push(transformToXML(xml.children[i]))
+        }
+    } else if (xml.children.length > 1 && !Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+        for (let i = 0; i < xml.children.length; i++) {
+            object[xml.children[i].nodeName] = transformToXML(xml.children[i]);
+        }
+
+    } else {
+        return xml.textContent;
+    }
+
+    return object;
+}
+console.log(resultObject);
+
+
+
+// transformToXML(parsedElement);
+
+// function transformToXML(xml) {
+//     let object = {};
+//     console.log(xml.parentNode);
+//     if (xml.parentNode.nodeType == 9) {
+//         if (Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+//             object[xml.children[0].nodeName] = [];
+//             for (let i = 0; i < xml.children.length; i++) {
+//                 object[xml.children[0].nodeName].push(transformToXML(xml.children[i]));
+//             }
+//         } else {
+//             for (let i = 0; i < xml.children.length; i++) {
+//                 object[xml.children[i].nodeName] = transformToXML(xml.children[i]);
+//             }
+//         }
+//         resultObject[xml.nodeName] = object;
+
+//     } else if (xml.children.length && Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+//         object[xml.children[0].nodeName] = [];
+//         for (let i = 0; i < xml.children.length; i++) {
+//             object[xml.children[0].nodeName].push(transformToXML(xml.children[i]));
+//         }
+//     } else if (xml.children.length > 1 && !Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+//         for (let x = 0; x < xml.children.length; x++) {
+//             object[xml.children[x].nodeName] = transformToXML(xml.children[x]);
+//         }
+//     } else {
+//         return xml.textContent;
+//     }
+//     return object;
+// }
+// console.log(resultObject);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let obj = {};
+// xmlDoc = xmlDoc.documentElement;
+// check(xmlDoc);
+
+// function check(xml) {
+//     let object = {};
+//     console.log(xml.children);
+//     if (xml.parentNode.nodeType == 9) {
+//         for (let i = 0; i < xml.children.length; i++) {
+//             object[xml.children[i].nodeName] = check(xml.children[i]);
+//         }
+//         obj[xml.nodeName] = object;
+
+//     } else if (xml.children.length > 1 && Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+//         obj[xml.nodeName] = [];
+
+//         for (let i = 0; i < xml.children.length; i++) {
+
+//             obj[xml.nodeName].push(check(xml.children[i]));
+//         }
+
+//     } else if (xml.children.length > 1 && !Array.from(xml.children).every((item, index, array) => item.nodeName == array[0].nodeName)) {
+//         for (let x = 0; x < xml.children.length; x++) {
+//             object[xml.children[x].nodeName] = check(xml.children[x]);
+//         }
+//     } 
+//     else {
+//         return xml.textContent;
+//     }
+//     return object;
+// }
+// console.log(obj);
